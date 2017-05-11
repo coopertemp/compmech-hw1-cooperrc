e3172fb9d27e5806245bf13b00535d4ecb9fa673
RCCstudent <ryan.c.cooper@uconn.edu>
2017-05-11 15:22:42 -0400

    Update lennard_jones.m

--- lennard_jones.m
+++ lennard_jones.m
@@ -1,4 +1,7 @@
 function E_LJ =lennard_jones(x,sigma,epsilon)
+  % E_LJ=lennard_jones(x,sigma,epsilon) 
+  % returns the energy based upon a lennard jones potential, E_LJ
+  % based upon the bond length, x, and two parameters,,sigma and epsilon
   E_LJ = 4*epsilon*((sigma./x).^12-(sigma./x).^6);
 end
 
